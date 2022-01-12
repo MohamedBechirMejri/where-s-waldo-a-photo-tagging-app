ed239470a6ec3d4908f440238d89c6422167780f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-12 17:06:02 +0100

    add character images

--- src/Components/Nav/Characters.js
+++ src/Components/Nav/Characters.js
@@ -1,5 +1,15 @@
+import mario from "../../Assets/Characters/Mario.webp";
+import luigi from "../../Assets/Characters/Luigi.png";
+
 const Characters = () => {
-  return <div className="w-1/3 h-full border-white marker: rounded-xl"></div>;
+  return (
+    <div className="flex items-center justify-center  h-full bg-[#ffffff62]   rounded-xl gap-4 w-1/3 overflow-scroll flex-wrap">
+      <img src={mario} alt="mario" className="h-[1.75em]" />
+      <img src={luigi} alt="mario" className="h-[1.75em]" />
+      <img src={mario} alt="mario" className="h-[1.75em]" />
+      <img src={mario} alt="mario" className="h-[1.75em]" />
+    </div>
+  );
 };
 
 export default Characters;
