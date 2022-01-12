edfc67cd1d7f0d5e10d98674eeb856e72f8b8ecd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-12 20:49:31 +0100

    add buttons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,13 @@ import Characters from "./Nav/Characters";
 const Nav = (props) => {
   return (
     <nav className="fixed top-0 flex items-center justify-around w-screen p-3 text-2xl font-extrabold text-white select-none h-fit">
-      {props.currentPage === "game" && <Characters />}
+      {props.currentPage === "game" ? (
+        <Characters />
+      ) : (
+        <button className="px-4 py-2 font-semibold text-white transition-all bg-transparent border border-white rounded hover:bg-white hover:text-black active:scale-95">
+          Credits
+        </button>
+      )}
       <h1
         className="text-3xl text-transparent uppercase transition-all active:scale-95 hover:text-white"
         style={{
@@ -13,7 +19,13 @@ const Nav = (props) => {
       >
         🦅I
       </h1>
-      {props.currentPage === "game" && <Timer />}
+      {props.currentPage === "game" ? (
+        <Timer />
+      ) : (
+        <button className="px-4 py-2 font-semibold text-white transition-all bg-transparent border border-white rounded hover:bg-white hover:text-black active:scale-95">
+          Top Scores
+        </button>
+      )}
     </nav>
   );
 };
