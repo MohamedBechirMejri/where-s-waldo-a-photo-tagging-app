469e9a9f53da304b6abdba278d7ea20c1a732ba3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-12 11:50:49 +0100

    Copilot: add more tests

--- src/Tests/GameBoard.test.js
+++ src/Tests/GameBoard.test.js
@@ -1,15 +1,14 @@
 import GameBoard from "../Components/GamePage/GameBoard";
-import dreamcast from "../../Assets/GameBoards/dreamcast.jpg";
-import gamecube from "../../Assets/GameBoards/gamecube.jpg";
-import n64 from "../../Assets/GameBoards/n64.jpg";
-import ps1 from "../../Assets/GameBoards/ps1.jpg";
-import ps2 from "../../Assets/GameBoards/ps2.jpg";
-import ps4 from "../../Assets/GameBoards/ps4.jpg";
-import xbox from "../../Assets/GameBoards/xbox.jpg";
-import xbox360 from "../../Assets/GameBoards/xbox360.jpg";
 
 describe("Images", () => {
   it(`should return image based on prop`, () => {
-    expect(GameBoard("n64")).toBe(n64);
+    expect(GameBoard("n64")).toBe("n64.jpg");
+    expect(GameBoard("ps1")).toBe("ps1.jpg");
+    expect(GameBoard("ps2")).toBe("ps2.jpg");
+    expect(GameBoard("ps4")).toBe("ps4.jpg");
+    expect(GameBoard("xbox")).toBe("xbox.jpg");
+    expect(GameBoard("xbox360")).toBe("xbox360.jpg");
+    expect(GameBoard("dreamcast")).toBe("dreamcast.jpg");
+    expect(GameBoard("gamecube")).toBe("gamecube.jpg");
   });
 });
