5335fe935d3a8b2dcc7c93f383010a75ca1865fa
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-12 12:39:20 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Nav from "./Components/Nav";
+import GamePage from "./Components/GamePage";
+import { useState } from "react";
 
 const App = () => {
+  const [game, setGame] = useState("n64");
+
   return (
     <div className="App">
       <Nav />
+      <GamePage game={game} />
     </div>
   );
 };
--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -1,5 +1,37 @@
-const GamePage = () => {
-  return <div></div>;
+import dreamcast from "../Assets/GameBoards/dreamcast.jpg";
+import gamecube from "../Assets/GameBoards/gamecube.jpg";
+import n64 from "../Assets/GameBoards/n64.jpg";
+import ps1 from "../Assets/GameBoards/ps1.jpg";
+import ps2 from "../Assets/GameBoards/ps2.jpg";
+import ps4 from "../Assets/GameBoards/ps4.jpg";
+import xbox from "../Assets/GameBoards/xbox.jpg";
+import xbox360 from "../Assets/GameBoards/xbox360.jpg";
+
+const GamePage = (props) => {
+  return (
+    <div>
+      <img
+        src={
+          props.game === "ps1"
+            ? ps1
+            : props.game === "ps2"
+            ? ps2
+            : props.game === "ps4"
+            ? ps4
+            : props.game === "xbox"
+            ? xbox
+            : props.game === "xbox360"
+            ? xbox360
+            : props.game === "gamecube"
+            ? gamecube
+            : props.game === "dreamcast"
+            ? dreamcast
+            : n64
+        }
+        alt={props.game}
+      />
+    </div>
+  );
 };
 
 export default GamePage;
