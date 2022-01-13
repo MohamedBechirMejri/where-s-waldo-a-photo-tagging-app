ad2368617558f658e2de612eec4ba721e4dd0cfe
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-13 18:54:16 +0100

    move characters imports to own file

---
+++ src/Assets/characters.js
@@ -0,0 +1,142 @@
+import mario from "./Characters/Mario.webp";
+import luigi from "./Characters/Luigi.png";
+
+const characters = [
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+  [
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+    {
+      src: luigi,
+      alt: "luigi",
+      isFound: false,
+    },
+    {
+      src: mario,
+      alt: "mario",
+      isFound: false,
+    },
+  ],
+];
+export default characters;
