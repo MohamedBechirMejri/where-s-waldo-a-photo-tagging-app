db2cbebd45cfd1e48e669ce10caa6c3cb045feb5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-13 15:04:51 +0100

    tweak images size on small screens

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,13 @@ import GameBoards from "../Assets/Gameboards";
 
 const Home = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center w-screen  py-[6em] gap-4 bg-[#272727]">
+    <div className="flex flex-wrap items-center justify-center w-screen  py-[6em] sm:gap-4 gap-1 bg-[#272727] ">
       {GameBoards.map((game, index) => {
         return (
           <img
             src={game}
             alt={`game-${index}`}
-            className="w-1/6 transition-all rounded-2xl hover:scale-105"
+            className="w-1/3 transition-all sm:w-1/5 rounded-2xl hover:scale-105"
           />
         );
       })}
