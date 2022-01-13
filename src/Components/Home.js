cc4578d89423800cbbc81cd012a312733fc21b1a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-13 16:11:54 +0100

    add console title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,16 +16,20 @@ const Home = (props) => {
     <div className="flex flex-wrap items-center justify-center w-screen  py-[6em] sm:gap-4 gap-1  ">
       {GameBoards.map((game, i) => {
         return (
-          <img
-            src={game}
-            alt={consoles[i]}
-            className="w-1/3 transition-all sm:w-1/5 rounded-2xl sm:hover:scale-105"
+          <div
             key={consoles[i]}
             onClick={() => {
               props.setGame(consoles[i]);
               props.setCurrentPage("game");
             }}
-          />
+            className="relative w-1/3 overflow-hidden transition-all sm:w-1/5 rounded-2xl sm:hover:scale-105"
+          >
+            <img src={game} alt={consoles[i]} />
+
+            <h1 className="absolute text-2xl font-extrabold text-white uppercase -translate-x-1/2 top-4 left-1/2">
+              {consoles[i]}
+            </h1>
+          </div>
         );
       })}
     </div>
