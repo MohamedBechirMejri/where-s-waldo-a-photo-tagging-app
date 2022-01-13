91fffaa90b9dbfc26866a2127c1ad59ddbf844dd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-13 18:53:01 +0100

    show characters based on image

--- src/Components/Nav/Characters.js
+++ src/Components/Nav/Characters.js
@@ -1,13 +1,17 @@
-import mario from "../../Assets/Characters/Mario.webp";
-import luigi from "../../Assets/Characters/Luigi.png";
 
-const Characters = () => {
+import characters from '../../Assets/characters';
+const Characters = (props) => {
   return (
     <div className="flex items-center justify-center  h-full bg-[#ffffff62]   rounded-xl gap-4 w-1/3 overflow-scroll flex-wrap">
-      <img src={mario} alt="mario" className="h-[1.75em]" draggable="false" />
-      <img src={luigi} alt="mario" className="h-[1.75em]" draggable="false" />
-      <img src={mario} alt="mario" className="h-[1.75em]" draggable="false" />
-      <img src={mario} alt="mario" className="h-[1.75em]" draggable="false" />
+      {characters[props.game.i].map((character) => (
+        <img
+          src={character.src}
+          alt={character.alt}
+          className="h-[1.75em]"
+          draggable="false"
+          key={character.alt}
+        />
+      ))}
     </div>
   );
 };
