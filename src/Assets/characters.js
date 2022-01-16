0b04b31e7ce7746bccccdce9c91d4da3c0ae554e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-16 20:51:12 +0100

    add max and min values for each character

--- src/Assets/characters.js
+++ src/Assets/characters.js
@@ -2,14 +2,17 @@ import mario from "./Characters/Mario.webp";
 import luigi from "./Characters/Luigi.png";
 
 const characters = [
+  // Dreamcast
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -17,19 +20,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // Gamecube
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -37,19 +45,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // N64
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 863,
+        XMin: 813,
+        yMax: 1710,
+        yMin: 1629,
       },
     },
     {
@@ -57,19 +70,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // PS1
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -77,19 +95,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // PS2
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -97,19 +120,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // PS4
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -117,19 +145,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // Xbox
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -137,19 +170,24 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
+  // XBOX360
   [
     {
       src: mario,
       alt: "mario",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
     {
@@ -157,10 +195,14 @@ const characters = [
       alt: "luigi",
       isFound: false,
       coords: {
-        x: 0,
-        y: 0,
+        xMax: 0,
+        XMin: 0,
+        yMax: 0,
+        yMin: 0,
       },
     },
   ],
 ];
 export default characters;
+
+//* Copilot is AMAZING!
