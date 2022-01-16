88cd3d90709f861cbfbef37b2833cebf2e86405b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-16 23:59:48 +0100

    style dropdown

--- src/Components/GamePage/Dropdown.js
+++ src/Components/GamePage/Dropdown.js
@@ -1,5 +1,11 @@
 const Dropdown = () => {
-  return <div></div>;
+  return (
+    <div className="absolute flex flex-col px-4 bg-white rounded right-1/2 top-1/2">
+      <button>test</button>
+      <button>test</button>
+      <button>test</button>
+    </div>
+  );
 };
 
 export default Dropdown;
