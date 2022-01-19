38928886ead0230992df14853942a73658c2e08a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-19 06:42:15 +0100

    add reveal animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       animation: {
         revealDD: "revealDD .2s forwards ",
+        reveal: "reveal .4s forwards ",
       },
       keyframes: {
         revealDD: {
           "0%": {
             opacity: 0,
             transform: "translate(0, -50%)",
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        reveal: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-50%, -30%)",
+          },
+          "50%": {
+            transform: "translate(-50%, -55%)",
           },
           "100%": {
             opacity: 1,
