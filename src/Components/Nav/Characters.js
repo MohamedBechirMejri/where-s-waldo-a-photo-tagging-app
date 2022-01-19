524496884b469eece5242a01d1bc3fca775546b5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-19 06:59:24 +0100

    blur bg

--- src/Components/Nav/Characters.js
+++ src/Components/Nav/Characters.js
@@ -1,6 +1,11 @@
 const Characters = (props) => {
   return (
-    <div className="flex items-center justify-center  h-full bg-[#ffffff62]   rounded-xl gap-4 w-1/3 overflow-scroll flex-wrap">
+    <div
+      className="flex items-center justify-center  h-full bg-[#ffffffa1]   rounded-xl gap-4 w-1/3 overflow-scroll flex-wrap"
+      style={{
+        backdropFilter: "blur(2.5em)",
+      }}
+    >
       {props.chars.map((character) => (
         <img
           src={character.src}
