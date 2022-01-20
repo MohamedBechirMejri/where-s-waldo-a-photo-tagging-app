6eaec2d1495a71e32f2943faad9aa3ba237a4a16
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-20 05:30:32 +0100

    send score to server

--- src/Components/GamePage/GameOver.js
+++ src/Components/GamePage/GameOver.js
@@ -1,11 +1,22 @@
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { useState } from "react";
+
 const GameOver = (props) => {
+  const [username, setUsername] = useState("");
+  const db = getFirestore();
+  const colRef = collection(db, props.console);
   return (
     <form
       className="fixed flex flex-col items-center justify-between gap-6 rounded-lg p-8 text-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit bg-[#ffffffa1] text-center font-bold capitalize ring-black ring animate-reveal"
       style={{
         backdropFilter: "blur(2.5em)",
       }}
-      onSubmit={() => {
+      onSubmit={(e) => {
+        e.preventDefault();
+        addDoc(colRef, {
+          name: username,
+          score: props.score,
+        });
         props.setCurrentPage("scores");
         props.setScore(0);
       }}
@@ -19,12 +30,11 @@ const GameOver = (props) => {
         placeholder="Enter Your Name"
         className="text-lg font-bold text-center capitalize bg-transparent outline-none placeholder:text-black"
         minLength={3}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
         required
       />
-      <button
-        className="py-3 font-bold uppercase transition-all bg-transparent rounded-lg px-7 ring-black ring-2 sm:hover:bg-black hover:text-white active:scale-95 hover:ring-0"
-        // onClick={}
-      >
+      <button className="py-3 font-bold uppercase transition-all bg-transparent rounded-lg px-7 ring-black ring-2 sm:hover:bg-black hover:text-white active:scale-95 hover:ring-0">
         Submit
       </button>
     </form>
